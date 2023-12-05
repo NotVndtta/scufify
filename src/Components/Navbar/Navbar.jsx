b0eaa10e8f8fafd3f7a782d3fefaabe988bd5eb7
NotVndtta <thenotalexey192@gmail.com>
2023-12-05 21:31:28 +0300

    navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <div className="nav-logo"> Scufify</div>
+      <u1 className="nav-menu">
+        <li>Home</li>
+        <li>Artists</li>
+        <li>Trends</li>
+        <li className='nav-contact'>Contact</li>
+      </u1>
+  </div>
+  )
+}
+
+
+export default Navbar
