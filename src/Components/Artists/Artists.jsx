550a36bdedeed8aa9c257a9bd4d199e7442fc167
NotVndtta <thenotalexey192@gmail.com>
2023-12-10 23:24:23 +0300

    Artists page

--- src/Components/Artists/Artists.jsx
+++ src/Components/Artists/Artists.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './artists.css'
+import mironus from '../../assets/mironchik.jpg'
 
 const Artists = () => {
   return (
     <div className='container'>
-       Мяу 
+      <div className='artist'>
+        <div className='artist_avatar'>
+          <img className='artist_img' src={mironus} alt='avatar'/>
+        </div>
+        <div className='artist_text'>
+          <div className='artist_name'> Oxxxymiron </div>
+          <div className='artist_description'></div>
+      </div>
+      </div>
     </div>
   )
 }
