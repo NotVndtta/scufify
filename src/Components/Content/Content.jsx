49615c2092fc4eb306689ffb830ddf967a679993
NotVndtta <thenotalexey192@gmail.com>
2024-03-23 22:16:48 +0300

    Artist page

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './content.css'
+import slavaAva from '../../assets/slava-ava.jpg'
 
 const Content = () => {
   return (
@@ -7,17 +8,19 @@ const Content = () => {
         <div className="singer">
           <div className="singer__header">
 
-            <div className="asinger__avatar">
-            Личико
+            <div className="singer__avatar">
+            <img src={slavaAva} alt='' className='singer__image'/>
             </div>
             <div className="singer__name">
+            <span> ИСПОЛНИТЕЛЬ </span> <br/>
             Слава КПСС
-            </div>
             <div className="singer__counter">
-            441 990
+            Количество слушателей: 441 990
             </div>
             <div className="singer__lable">
+            <span> Лейбл </span> <br/>
             Антихайп
+            </div>
             </div>
           </div>
           <div className="singer__navigation">
@@ -25,6 +28,23 @@ const Content = () => {
           </div>
           <div className="singer__content">
             Контент
+    <iframe 
+      frameborder="0" 
+      style={{ border: "none", width: "100%", height: "500px" }} 
+      width="100%" 
+      height="500" 
+      src="https://music.yandex.ru/iframe/album/12659818/">
+      Слушайте <a href='https://music.yandex.ru/album/12659818'>ЧУДОВИЩЕ ПОГУБИВШЕЕ МИР</a> — <a href='https://music.yandex.ru/artist/4622988'>Слава КПСС</a> на Яндекс Музыке
+    </iframe>
+
+    <iframe frameborder="0" style={{ border: "none", width: "100%", height: "500px" }} 
+      width="438" 
+      height="570" 
+      src="https://music.yandex.ru/iframe/album/8950811">Слушайте 
+      <a href='https://music.yandex.ru/album/8950811'>ОТТЕНКИ БАРДА</a> — 
+      <a href='https://music.yandex.ru/artist/4622988'>
+      Слава КПСС</a> на Яндекс Музыке</iframe>
+
           </div>
 
         </div>
